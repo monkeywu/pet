311d92d9f7d6ff925e69f0fa9822143f0656ae01
james <james20209@msn.com>
2017-05-30 21:54:40 +0800

    z

--- js/main.js
+++ js/main.js
@@ -201,8 +201,8 @@ $('.list').on('click','.gomap',function(e){
     e.preventDefault();
     $('#map').css('left','25%');
     $('.close_map').css('display','block');
-        if($('.gomap:first').text() == mapdata[i].address){
-            for(i=0;i<mapdata.length;i++){
+    for(i=0;i<mapdata.length;i++){
+        if(this.text() == mapdata[i].address){
             function initMap() {
             var lat = mapdata[i].position.lat;
             var lng = mapdata[i].position.lng;
@@ -218,6 +218,7 @@ $('.list').on('click','.gomap',function(e){
             }
             initMap();
         }
+
     }
 })
 
